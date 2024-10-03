cf0c648fb5782681520c89f6d0f79b242b4647f3
bhavesh1234785 <ganesunibhavesh8@gmail.com>
2024-10-03 20:42:31 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,34 @@
+def add(P,Q):
+    #this function is used for adding two numbers
+    return P+Q
+def subtract(P,Q):
+    #this function is used for subtracting two numbers
+    return P-Q
+def multiply(P,Q):
+    #this function is used for multiplying two numbers
+    return P*Q
+def divide(P,Q):
+    #this function is used for dividing two numbers
+    return P/Q
+#now we will take inputs from the user
+print("please select the operation.")
+print("a.Add")
+print("b.subtract")
+print("c.multiply")
+print("d.divide")
+
+choice=input("Please enter choice(a/b/c/d): ")
+
+num_1=int (input ("please enter the first number: "))
+num_2=int (input ("please enter the second number: "))
+
+if choice=='a':
+    print(num_1,"+",num_2,"=",add(num_1,num_2))
+elif choice=='b':
+    print(num_1,"-",num_2,"=",subtract(num_1,num_2))
+elif choice=='c':
+    print(num_1,"*",num_2,"=",multiply(num_1,num_2))
+elif choice=='d':
+    print(num_1,"/",num_2,"=",divide(num_1,num_2))
+else:
+    print("this is an invalid input")
